dafa39156c6226ecdc6b911d1840170d57115f2c
EC2 Default User <ec2-user@ip-172-31-2-173.us-east-2.compute.internal>
2018-09-29 06:17:17 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit,
                                         :update, :destroy]
@@ -11,7 +12,8 @@ class UsersController < ApplicationController
 
   # GET /users/:id
   def show
-    @user = User.find(params[:id])
+    @user       = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     # => app/views/users/show.html.erb
     # debugger
   end
@@ -26,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save # => Validation
+      # Sucess
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
@@ -69,15 +72,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :name, :email, :password, 
         :password_confirmation)
-    end
-  
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
     
     # 正しいユーザーかどうか確認
